fb3d4c927c474423da482677772546b41acbacb4
Gabriel <danquahbadugabriel@gmail.com>
2025-09-29 22:10:41 +0000

    Added STATIC_ROOT for static files

--- BG_Restaurant/settings.py
+++ BG_Restaurant/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-hx2mjln*ido6w+sx_zf1ndnhu0ix$n9eti$a!s9e-uz0ueviz)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['NanaDanquah.pythonanywhere.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
@@ -122,6 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'menu/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATICFILES_DIRS = [
     'menu/static',
